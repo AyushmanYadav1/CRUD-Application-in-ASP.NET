65eb2ef858012dcfb0d30c4bd9d5914c195eb891
AyushmanYadav1 <102944492+AyushmanYadav1@users.noreply.github.com>
2022-04-04 11:54:18 +0530

    Add files via upload

---
+++ CommanCode/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TableView.CommanCode
+{
+    public interface IEmployee
+    {
+        void Employee();
+    }
+
+
+    public class EmployeeClass : IEmployee
+    {
+        public void Employee()
+        {
+            
+        }
+        
+    }
+}
